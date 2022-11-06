e1f4f6233767dbbb97e7435b074a758f081af04d
Lucas Arruda Vidal <lucas.2019107665@unicap.br>
2022-11-05 22:14:17 -0300

    fixes

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,10 +1,10 @@
 
 public class InsertionSort {
     public void insertionSort(int arr[], int n) {
-        int i, ult, j;
-        for (i = 1; i < n; i++) {
-            ult = arr[i];
-            j = i - 1;
+        
+        for (int i = 1; i < n; i++) {
+            int ult = arr[i];
+            int j = i - 1;
             while (j >= 0 && arr[j] > ult) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
