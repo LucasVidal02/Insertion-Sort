673f9ea6c0d723bc259358df4a151a0bc1e76c9f
Lucas Arruda Vidal <lucas.2019107665@unicap.br>
2022-11-05 22:00:50 -0300

    algorithm implementation (iterative and recursive)

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,3 +1,15 @@
-public class InsertionSort{
-    
+
+public class InsertionSort {
+    public void insertionSort(int arr[], int n) {
+        int i, ult, j;
+        for (i = 1; i < n; i++) {
+            ult = arr[i];
+            j = i - 1;
+            while (j >= 0 && arr[j] > ult) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = ult;
+        }
+    }
 }
--- InsertionSortRec.java
+++ InsertionSortRec.java
@@ -1,3 +1,25 @@
 public class InsertionSortRec {
-    
+    public void insertionSortRecursivo(int arr[], int n){
+
+        //caso base
+        if(n <= 1){
+            return;
+        }
+
+        //ordenar os primeiros n-1 elementos
+        insertionSortRecursivo(arr, n-1);
+
+        //inserir o último elemento na posição correta
+        int ult = arr[n-1];
+        int j = n - 2;
+        
+        //mover os elementos do array, que são maior que a chave, uma posição à frente da posição atual.
+        while (j >= 0 && arr[j] > ult){
+            arr[j + 1]  = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = ult;
+
+    }
 }
